6916a94dd896deb976cc05d3f4502226fa2e68d2
Hegyi Hajnalka <hajhegyi@hegyis-mbp.misys.global.ad>
2015-12-14 11:55:07 +0100

    Initial Commit

---
+++ AnimatedMatchismo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnimatedMatchismo
+//
+//  Created by Hegyi Hajnalka on 14/12/15.
+//  Copyright © 2015 Hegyi Hajnalka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AnimatedMatchismo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AnimatedMatchismo
+//
+//  Created by Hegyi Hajnalka on 14/12/15.
+//  Copyright © 2015 Hegyi Hajnalka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
